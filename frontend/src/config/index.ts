6c74e73d0520400af6b423c74217c441f85c65c6
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2022-11-16 07:50:49 +0100

    refactor to use env variables

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
+import dotenv from 'dotenv';
 
 export default axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: process.env.NODE_ENV === "production" ? process.env.BACKEND_URL : 'http://localhost:3000/',
 })
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 
-module.exports = {
+module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    // proxy: 'http://localhost:3000',
     proxy: {
       '^/api': {
         target: 'http://localhost:3000',
@@ -12,4 +11,4 @@ module.exports = {
       },
     },
   },
-};
+});
