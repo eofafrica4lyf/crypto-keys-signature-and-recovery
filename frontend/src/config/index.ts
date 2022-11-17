d8197282c02d79f968291098043b335f6672244a
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2022-11-17 03:19:13 +0100

    resolve frontend issue with dotenv package

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import dotenv from 'dotenv';
 
 export default axios.create({
     baseURL: process.env.NODE_ENV === "production" ? process.env.BACKEND_URL : 'http://localhost:3000/',
